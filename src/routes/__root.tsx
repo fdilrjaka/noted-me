f191b7cbe8b09713a09af248504e2b24568a4018
fdilrjaka <fadhil.ramadhan386@gmail.com>
2026-08-31 19:03:32 +0700

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -14,6 +14,7 @@ import { Toaster } from "sonner";
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { ServiceWorkerRegistrar } from "@/components/noteme/SyncEngine";
+import { applyTheme, readTheme } from "@/lib/noteme/theme";
 
 function NotFoundComponent() {
   return (
@@ -120,6 +121,10 @@ function RootShell({ children }: { children: ReactNode }) {
 
 function RootComponent() {
   const { queryClient } = Route.useRouteContext();
+
+  useEffect(() => {
+    applyTheme(readTheme());
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
