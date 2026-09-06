83c80431a60c2249f315079c2b96ac3eb66d25d4
fdilrjaka <fadhil.ramadhan386@gmail.com>
2026-09-06 22:14:51 +0700

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -80,7 +80,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         name: "viewport",
         content: "width=device-width, initial-scale=1, viewport-fit=cover, maximum-scale=1",
       },
-      { title: "NoteMe — Catatan Mata Kuliah" },
+      { title: "NoteMe" },
       {
         name: "description",
         content:
